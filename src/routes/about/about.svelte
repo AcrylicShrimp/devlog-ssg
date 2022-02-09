9f9bcb5b083acaa24f6d39504c7c1a5ff451ac5c
AcrylicShrimp <led789zxpp@naver.com>
2022-02-09 14:23:16 +0900

    feat: add a profile image to the about page.

--- src/routes/about/about.svelte
+++ src/routes/about/about.svelte
@@ -15,3 +15,4 @@
 	<p>This website is a personal blog of AcrylicShrimp. Most of posts are thinkings and works about programming.</p>
 	<p>I'm a developer who interested in graphics and compilers. And I also love games too!</p>
 </div>
+<img src="/img/profile.png" alt="acrylicshrimp profile" class="block mx-auto my-6 rounded max-w-full" />

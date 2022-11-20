948f37b2001890fdae7a323d5eb3cb8d88db1038
AcrylicShrimp <led789zxpp@naver.com>
2022-11-20 20:26:17 +0900

    feat: update the about page.

--- src/routes/about/about.svelte
+++ src/routes/about/about.svelte
@@ -12,8 +12,8 @@
 		<h1 class="font-bold text-xl sm:text-3xl text-lightblue">About</h1>
 	</header>
 	<div class="mt-4 text-sm sm:text-lg text-gray">
-		<p>This website is a personal blog of AcrylicShrimp. Most of posts are thinkings and works about programming.</p>
-		<p>I'm a developer who interested in graphics and compilers. And I also love games too!</p>
+		<p>Hello! My name is AcrylicShrimp. I'm a software engineer who interested in graphics and compilers.</p>
+		<p>This website is my personal blog. Most of posts are thinkings and works about my fields.</p>
 	</div>
 	<img src="/img/profile.png" alt="acrylicshrimp profile" class="block mx-auto my-6 rounded max-w-full" />
 </article>

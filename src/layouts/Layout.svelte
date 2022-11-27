2f98a1c2bb07ec2debe7b1d607ed60149c440d99
AcrylicShrimp <led789zxpp@naver.com>
2022-11-28 01:40:28 +0900

    fix: correct the SEO meta tags.

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -2,7 +2,7 @@
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
 
-	export let helpers, templateHtml;
+	export let helpers, templateHtml, request, settings;
 </script>
 
 <style global lang="postcss">
@@ -13,15 +13,15 @@
 
 <svelte:head>
 	<!-- Facebook Meta Tags -->
-	<meta property="og:url" content="https://blog.ashrimp.dev/index.html" />
+	<meta property="og:site_name" content={settings.title} />
 	<meta property="og:type" content="website" />
 	<meta property="og:image" content="https://blog.ashrimp.dev/img/profile.png" />
-
+	<meta property="og:url" content={`${settings.origin}${request.permalink}index.html`} />
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:domain" content="blog.ashrimp.dev" />
-	<meta name="twitter:url" content="https://blog.ashrimp.dev/" />
 	<meta name="twitter:image" content="https://blog.ashrimp.dev/img/profile.png" />
+	<meta name="twitter:url" content={`${settings.origin}${request.permalink}index.html`} />
 </svelte:head>
 
 <div class="p-1 py-4 sm:p-6 sm:pt-12 md:max-w-screen-lg md:mx-auto">
